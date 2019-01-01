83a0cf56951bae4ac2caaf82a0e495b211d6ad2a
Weitle <18602207310@163.com>
2019-01-01 09:06:40 -0500

    修改 insert_by_id

--- Chapter1/mongo/insert_by_id.py
+++ Chapter1/mongo/insert_by_id.py
@@ -1,14 +1,14 @@
 import pymongo
-client = pymongo.MongoClient('mongdodb://localhost:27017')
+client = pymongo.MongoClient('mongodb://localhost:27017/')
 runoob = client['runoob']
-sites2 = runoob['sites']
+sites2 = runoob['sites2']
 sites = [
-    { "_id": 1, "name": "RUNOOB", "cn_name": "菜鸟教程"},
-    { "_id": 2, "name": "Google", "address": "Google 搜索"},
-    { "_id": 3, "name": "Facebook", "address": "脸书"},
-    { "_id": 4, "name": "Taobao", "address": "淘宝"},
-    { "_id": 5, "name": "Zhihu", "address": "知乎"}
+    { "_id": 11, "name": "RUNOOB", "cn_name": "菜鸟教程"},
+    { "_id": 12, "name": "Google", "address": "Google 搜索"},
+    { "_id": 13, "name": "Facebook", "address": "脸书"},
+    { "_id": 14, "name": "Taobao", "address": "淘宝"},
+    { "_id": 15, "name": "Zhihu", "address": "知乎"}
 ]
 
-result = sites.insert_many(sites)
-print(result.inserted_id)
+result = sites2.insert_many(sites)
+print(result.inserted_ids)
